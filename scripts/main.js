c9f8bc9f9d9a1eca94c6e1ab1e71eabac0798681
Rebecca Feng <beckyfeng08@berkeley.edu>
2024-04-24 14:12:40 -0700

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GUI } from 'dat.gui'
+
 const container = document.body;
   
 function main() {
   
-
     //setting up the scene
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
@@ -14,7 +15,9 @@ function main() {
     
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( container.clientWidth, container.clientHeight);
-  
+    document.body.appendChild(renderer.domElement);
+
+
     //setting up orbitcontrols
     const controls = new OrbitControls( camera, renderer.domElement );
     
@@ -24,7 +27,28 @@ function main() {
     const cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
-    document.body.appendChild(renderer.domElement);
+    //GUI controls dat.gui
+    const gui = new GUI();
+
+    gui.add(cube.material, "wireframe");
+    //color picker
+    let color = {
+        r: 0,
+        g: 0,
+        b: 0
+    };
+    let palette = {
+        color: [0,255,255]
+    }
+    let folderRGB = gui.addFolder("RGB");
+    folderRGB.addColor(palette, 'color').onChange(function(value) {
+        color.r = value[0]/255;
+        color.g = value[1]/255;
+        color.b = value[2]/255;
+        console.log(color);
+    })
+    
+    
     //update frame
     animate();
     function animate() {
