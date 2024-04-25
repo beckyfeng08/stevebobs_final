513e4c04d5a5b86e3e8e5804030001131db84a54
Olivia Xie <olivia.xie@berkeley.edu>
2024-04-24 18:47:10 -0700

    modify toolbar

--- drawingapp/script.js
+++ drawingapp/script.js
@@ -25,29 +25,29 @@ window.addEventListener("load", () => {
     canvas.height = canvas.offsetHeight;
     setCanvasBackground();
 });
-const drawRect = (e) => {
-    // if fillColor isn't checked draw a rect with border else draw rect with background
-    if (!fillColor.checked) {
-        // creating circle according to the mouse pointer
-        return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
-    }
-    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
-}
-const drawCircle = (e) => {
-    ctx.beginPath(); // creating new path to draw circle
-    // getting radius for circle according to the mouse pointer
-    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
-    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI); // creating circle according to the mouse pointer
-    fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill circle else draw border circle
-}
-const drawTriangle = (e) => {
-    ctx.beginPath(); // creating new path to draw circle
-    ctx.moveTo(prevMouseX, prevMouseY); // moving triangle to the mouse pointer
-    ctx.lineTo(e.offsetX, e.offsetY); // creating first line according to the mouse pointer
-    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY); // creating bottom line of triangle
-    ctx.closePath(); // closing path of a triangle so the third line draw automatically
-    fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill triangle else draw border
-}
+// const drawRect = (e) => {
+//     // if fillColor isn't checked draw a rect with border else draw rect with background
+//     if (!fillColor.checked) {
+//         // creating circle according to the mouse pointer
+//         return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+//     }
+//     ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+// }
+// const drawCircle = (e) => {
+//     ctx.beginPath(); // creating new path to draw circle
+//     // getting radius for circle according to the mouse pointer
+//     let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+//     ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI); // creating circle according to the mouse pointer
+//     fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill circle else draw border circle
+// }
+// const drawTriangle = (e) => {
+//     ctx.beginPath(); // creating new path to draw circle
+//     ctx.moveTo(prevMouseX, prevMouseY); // moving triangle to the mouse pointer
+//     ctx.lineTo(e.offsetX, e.offsetY); // creating first line according to the mouse pointer
+//     ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY); // creating bottom line of triangle
+//     ctx.closePath(); // closing path of a triangle so the third line draw automatically
+//     fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill triangle else draw border
+// }
 const startDraw = (e) => {
     isDrawing = true;
     prevMouseX = e.offsetX; // passing current mouseX position as prevMouseX value
@@ -68,13 +68,14 @@ const drawing = (e) => {
         ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
         ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
         ctx.stroke(); // drawing/filling line with color
-    } else if (selectedTool === "rectangle") {
-        drawRect(e);
-    } else if (selectedTool === "circle") {
-        drawCircle(e);
-    } else {
-        drawTriangle(e);
-    }
+    } 
+    // else if (selectedTool === "rectangle") {
+    //     drawRect(e);
+    // } else if (selectedTool === "circle") {
+    //     drawCircle(e);
+    // } else {
+    //     drawTriangle(e);
+    // }
 }
 toolBtns.forEach(btn => {
     btn.addEventListener("click", () => { // adding click event to all tool option
