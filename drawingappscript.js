8199ca298c12eec259049c68e73141be48515f20
Rebecca Feng <beckyfeng08@berkeley.edu>
2024-04-27 14:34:49 -0700

    got the drawing interface to work with a sube next to it

--- drawingappscript.js
+++ drawingappscript.js
@@ -1,4 +1,5 @@
-const canvas = document.querySelector("canvas"),
+console.log(document.getElementById("drawingapp"))
+const canvas = document.getElementById("drawingapp"),
     toolBtns = document.querySelectorAll(".tool"),
     fillColor = document.querySelector("#fill-color"),
     sizeSlider = document.querySelector("#size-slider"),
@@ -7,6 +8,7 @@ const canvas = document.querySelector("canvas"),
     clearCanvas = document.querySelector(".clear-canvas"),
     saveImg = document.querySelector(".save-img"),
     ctx = canvas.getContext("2d");
+
 // global variables with default value
 let prevMouseX, prevMouseY, snapshot,
     isDrawing = false,
--- scripts/main.js
+++ scripts/main.js
@@ -31,6 +31,139 @@ let meshesready = false;
 let texturesready = true;
 
 let Brush;
+
+
+//calls
+preload().then(drawingapp).then(animate);
+
+async function drawingapp() {
+    
+    const canvas = document.getElementById("drawingapp"),
+        toolBtns = document.querySelectorAll(".tool"),
+        fillColor = document.querySelector("#fill-color"),
+        sizeSlider = document.querySelector("#size-slider"),
+        colorBtns = document.querySelectorAll(".colors .option"),
+        colorPicker = document.querySelector("#color-picker"),
+        clearCanvas = document.querySelector(".clear-canvas"),
+        saveImg = document.querySelector(".save-img");
+        const ctx = canvas.getContext("2d");
+        console.log(ctx)
+    // global variables with default value
+    
+    let prevMouseX, prevMouseY, snapshot,
+        isDrawing = false,
+        selectedTool = "brush",
+        brushWidth = 5,
+        selectedColor = "#000";
+    const setCanvasBackground = () => {
+        // setting whole canvas background to white, so the downloaded img background will be white
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = selectedColor; // setting fillstyle back to the selectedColor, it'll be the brush color
+    }
+    window.addEventListener("load", () => {
+        
+        // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+        setCanvasBackground();
+    });
+
+    window.addEventListener("resize", () => {
+        // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+        // setCanvasBackground();
+    });
+  
+
+    const startDraw = (e) => {
+        isDrawing = true;
+        prevMouseX = e.offsetX; // passing current mouseX position as prevMouseX value
+        prevMouseY = e.offsetY; // passing current mouseY position as prevMouseY value
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        // ctx.arc(prevMouseX, prevMouseY, brushWidth/2.0, 0, 2*Math.PI); // for single click paint case
+        ctx.fillStyle = selectedColor; // passing selectedColor as fill style
+        // ctx.fill();
+        ctx.beginPath(); // creating new path to draw
+
+        ctx.lineWidth = brushWidth; // passing brushSize as line width
+        // ctx.strokeStyle = selectedColor; // passing selectedColor as stroke style
+        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
+
+        ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
+        ctx.stroke(); // drawing/filling line with color
+
+        // copying canvas data & passing as snapshot value.. this avoids dragging the image
+        snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    }
+    const drawing = (e) => {
+        if (!isDrawing) return; // if isDrawing is false return from here
+        ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on to this canvas
+        if (selectedTool === "brush" || selectedTool === "eraser") {
+            ctx.lineCap = "round";
+            ctx.lineJoin = "round";
+            // if selected tool is eraser then set strokeStyle to white
+            // to paint white color on to the existing canvas content else set the stroke color to selected color
+            ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
+            ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
+            ctx.stroke(); // drawing/filling line with color
+        }
+   
+    }
+    toolBtns.forEach(btn => {
+        btn.addEventListener("click", () => { // adding click event to all tool option
+            // removing active class from the previous option and adding on current clicked option
+            document.querySelector(".options .active").classList.remove("active");
+            btn.classList.add("active");
+            selectedTool = btn.id;
+        });
+    });
+    sizeSlider.addEventListener("change", () => { // passing slider value as brushSize
+        brushWidth = sizeSlider.value;
+    });
+    // brush size cursor display change
+    document.onmousemove = function(e){
+        var circle = document.getElementById("circle");
+        circle.style.top = e.clientY+"px";
+        circle.style.left = e.clientX+"px";
+        circle.style.width = brushWidth+"px";
+        circle.style.height = brushWidth+"px";
+    }
+    colorBtns.forEach(btn => {
+        btn.addEventListener("click", () => { // adding click event to all color button
+            // removing selected class from the previous option and adding on current clicked option
+            document.querySelector(".options .selected").classList.remove("selected");
+            btn.classList.add("selected");
+            // passing selected btn background color as selectedColor value
+            selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+        });
+    });
+    colorPicker.addEventListener("change", () => {
+        // passing picked color value from color picker to last color btn background
+        colorPicker.parentElement.style.background = colorPicker.value;
+        colorPicker.parentElement.click();
+    });
+    clearCanvas.addEventListener("click", () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing whole canvas
+        setCanvasBackground();
+    });
+    saveImg.addEventListener("click", () => {
+        const link = document.createElement("a"); // creating <a> element
+        link.download = `${Date.now()}.jpg`; // passing current date as link download value
+        link.href = canvas.toDataURL(); // passing canvasData as link href value
+        link.click(); // clicking link to download image
+    });
+    canvas.addEventListener("mousedown", startDraw);
+    canvas.addEventListener("mousemove", drawing);
+    canvas.addEventListener("mouseup", () => {
+        isDrawing = false;
+    });
+
+}
+
 //main functions
 async function preload() {
     //textures is a list of textures available to use, and populates the textures list
@@ -79,32 +212,39 @@ function addLights() {
 }
 function addMeshes() {
     //rendering a simple cube for now (CHANGE TO A 3D MESH)
-    const loader = new GLTFLoader();
-    //loading the cow
-    loader.load(
-        '../models/cow_unwrapped.glb',
-        function (gltf) {
-            const modelGeometry = gltf.scene.children[0].geometry;
-            var loader = new THREE.TextureLoader();
-            const material = new THREE.MeshPhongMaterial( {
-                side: THREE.DoubleSide,
-                flatShading: true,
 
-            } );
-            const modelMesh = new THREE.Mesh(modelGeometry,  material);
-            modelMesh.scale.set(5, 5, 5);
-            scene.add(modelMesh);
-            meshes.push(modelMesh); 
-        },
-        // if 100% means loaded
-        function (xhr) {
-            console.log("Mesh loaded successfully");
-            meshesready = true;
-        },
-        // an error callback
-        function (error) {
-            console.error('An error happened', error);
-        });
+    //lets just do a cube to debug for now
+    var material = new THREE.MeshBasicMaterial();
+
+	var mesh = new THREE.Mesh( new THREE.BoxGeometry( 5,5,5 ), material );
+    scene.add(mesh);
+    meshes.push(mesh); 
+    // const loader = new GLTFLoader();
+    // //loading the cow
+    // loader.load(
+    //     '../models/cow_unwrapped.glb',
+    //     function (gltf) {
+    //         const modelGeometry = gltf.scene.children[0].geometry;
+    //         var loader = new THREE.TextureLoader();
+    //         const material = new THREE.MeshPhongMaterial( {
+    //             side: THREE.DoubleSide,
+    //             flatShading: true,
+
+    //         } );
+    //         const modelMesh = new THREE.Mesh(modelGeometry,  material);
+    //         modelMesh.scale.set(5, 5, 5);
+    //         scene.add(modelMesh);
+    //         meshes.push(modelMesh); 
+    //     },
+    //     // if 100% means loaded
+    //     function (xhr) {
+    //         console.log("Mesh loaded successfully");
+    //         meshesready = true;
+    //     },
+    //     // an error callback
+    //     function (error) {
+    //         console.error('An error happened', error);
+    //     });
 
 }
 
@@ -181,8 +321,6 @@ window.addEventListener("resize", function() {
     camera.updateProjectionMatrix();
 });
 
-//calls
-preload().then(animate);
 
 //error functions
 function assertListsSameSize(list1, list2) {
